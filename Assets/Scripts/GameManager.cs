71576edc948286b14fedfb17edfbc0e35261436a
DESKTOP-VR3A6C3\kimho <kimhongyeol036@naver.com>
2020-10-10 13:15:25 +0900

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,17 @@ public class GameManager : MonoSingleton<GameManager>
 {
     public bool downCheck = false;
     public float speed = 1000f;
-
    
+}
+
+public class game_manager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 }
