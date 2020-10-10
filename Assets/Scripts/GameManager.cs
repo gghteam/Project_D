25e0d073fb3e7605f72a91a18c125907d67fbcea
DESKTOP-VR3A6C3\kimho <kimhongyeol036@naver.com>
2020-10-10 23:46:46 +0900

    게임 종료 버튼 생성 / 구현 설정 닫기버튼 재생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,7 @@ using UnityEngine;
 public class GameManager : MonoSingleton<GameManager>
 {
     public bool downCheck = false;
+
     public float speed = 1000f;
-   
-}
 
-public class game_manager : MonoBehaviour
-{
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
 }
