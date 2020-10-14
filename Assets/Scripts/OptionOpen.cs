0fdef4f3f940c03e60d81d85a11d8d852a29a23f
DESKTOP-VR3A6C3\kimho <kimhongyeol036@naver.com>
2020-10-14 17:48:14 +0900

    스테이지 1 설정 구현

---
+++ Assets/Scripts/OptionOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionOpen : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void Open1()
+    {
+        Panel.SetActive(true);
+    }
+    public void Close1()
+    {
+        Panel.SetActive(false);
+    }
+}
