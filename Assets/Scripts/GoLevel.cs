730c99a0b5c1af7c4c65cd8a4fd437f3cb36b9df
DESKTOP-VR3A6C3\kimho <kimhongyeol036@naver.com>
2020-10-16 12:42:21 +0900

    모든 설정 UI 수정 및 레벨로 돌아가기 추가

---
+++ Assets/Scripts/GoLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoLevel : MonoBehaviour
+{
+
+    public GameObject PauseUI;
+
+    public void Restaret()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
