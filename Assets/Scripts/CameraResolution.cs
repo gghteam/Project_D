e36fb480ea72c425e3f8b42718fcbd31252f5bcb
hwanjuson <hwanjuson@gmail.com>
2020-10-19 08:40:05 +0900

    성후가 알려준거로 바ㅜ꺼봄

--- Assets/Scripts/CameraResolution.cs
+++ Assets/Scripts/CameraResolution.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class CameraResolution : MonoBehaviour
 {
-    private void Awake()
+    public Vector2 standard_resolution;
+    public Camera camera1;
+    public bool isLandScape;
+
+    private float rate;
+
+    private void Start()
     {
-        Camera camera = GetComponent<Camera>();
-        Rect rect = camera.rect;
-        float scaleheight = ((float)Screen.width / Screen.height) / ((float)16 / 9);
-        float scalewidth = 1f / scaleheight;
-        if(scaleheight < 1)
+        rate = standard_resolution.y / standard_resolution.x;
+    }
+    private void Update()
+    {
+        Setting();
+    }
+    private void Setting()
+    {
+        int size = Screen.height - Screen.width;
+        bool isScreenWrong = (size > 0 && isLandScape) || (size < 0 && !isLandScape);
+        if (isScreenWrong) return;
+        float mobile_rate = (float)Screen.height / (float)Screen.width;
+        //위아래가 길어지는 경우
+        if(mobile_rate > rate)
         {
-            rect.height = scaleheight;
-            rect.y = (1f - scaleheight) / 2f;
+            float h = rate / mobile_rate;
+            camera1.rect = new Rect(0, (1 - h) / 2, 1, h);
         }
+        //좌우로 늘어나는 경우
         else
         {
-            rect.width = scalewidth;
-            rect.x = (1f - scalewidth) / 2f;
+            float w = mobile_rate / rate;
+            camera1.rect = new Rect((1 - w) / 2, 0, w, 1);
         }
-        camera.rect = rect;
+        this.enabled = false;
     }
+
 }
