0ab72484574d4b454ce1e2d813ec7752fd039649
hwanjuson <hwanjuson@gmail.com>
2020-11-11 18:28:10 +0900

    뒤로가기 버튼 적용

---
+++ Assets/Scripts/BackQuit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackQuit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                // 할꺼 하셈
+                Application.Quit();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
