7288a967769c54b6a79a0632ffbe3a5e11c09615
DESKTOP-VR3A6C3\kimho <kimhongyeol036@naver.com>
2020-11-17 10:17:24 +0900

    다시하기 크기 조정 및 인게임 사운드 ON OFF

---
+++ Assets/Scripts/InGameSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSound : MonoBehaviour
+{
+    private SoundOnOff s;
+
+    void Start()
+    {
+
+        s = GameObject.Find("SoundManager").GetComponent<SoundOnOff>();
+    }
+
+    public void OnclickSoundButton2()
+    {
+        s.OnClickSoundButton();
+    }
+
+    void Update()
+    {
+        
+    }
+}
