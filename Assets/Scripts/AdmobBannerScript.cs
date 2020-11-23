036708956c9e65955d23d24eaf86bd6750d3f2d2
hwanjuson <hwanjuson@gmail.com>
2020-11-23 19:19:18 +0900

    레벨씬 배너광고 추가

---
+++ Assets/Scripts/AdmobBannerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdmobBannerScript : MonoBehaviour
+{
+    static bool isAdsBannerLoaded = false;
+    
+    void Start()
+    {
+        if (!isAdsBannerLoaded)
+            RequestBanner();
+    }
+
+    //광고 요청 Method
+    private void RequestBanner()
+    {
+        //Test Banner ID : ca-app-pub-3940256099942544/6300978111
+        //여러분들의 광고 ID가 들어갈 곳입니다.
+        //ca-app-pub-3462718233988228/6832749500
+        string BannerID = "ca-app-pub-3462718233988228/6832749500";
+        BannerView bannerview = new BannerView(BannerID, AdSize.SmartBanner, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerview.LoadAd(request);
+        isAdsBannerLoaded = true;
+    }
+}
