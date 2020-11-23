4729a1ebe449cc5c6c79954ccbdb55c735607168
hwanjuson <hwanjuson@gmail.com>
2020-11-23 19:35:40 +0900

    현재 스테이지 레벨 표시

---
+++ Assets/Scripts/currentSceneMg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class currentSceneMg : MonoBehaviour
+{
+    public Text sceneText;
+
+    [System.Obsolete]
+    void Update()
+    {
+        sceneText.text = Application.loadedLevelName;
+    }
+}
